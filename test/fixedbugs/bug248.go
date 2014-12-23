32f81d583a70af3bc714018c214340e58fe45a77
Josh Bleecher Snyder <josharian@gmail.com>
2014-12-23 13:13:04 -0800

    test: remove errchk usage
    Copy/paste the implementation of errchk in run.go
    into the two places where it is used.
    
    This enables these tests to be run on Windows and
    on Plan 9. It also removes the only remaining
    usage of errchk in the main repo.
    
    Change-Id: Icac1347619eab831307f348cdd803bff1747aa53

--- test/fixedbugs/bug248.go
+++ test/fixedbugs/bug248.go
@@ -1,4 +1,4 @@
-// +build !nacl,!plan9,!windows
+// +build !nacl
 // run
 
 // Copyright 2009 The Go Authors. All rights reserved.
@@ -8,19 +8,22 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"go/build"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	a, err := build.ArchChar(build.Default.GOARCH)
-	check(err)
-
-	// TODO: If we get rid of errchk, re-enable this test on Windows.
-	errchk, err := filepath.Abs("errchk")
 	check(err)
 
 	err = os.Chdir(filepath.Join("fixedbugs", "bug248.dir"))
@@ -29,7 +32,7 @@ func main() {
 	run("go", "tool", a+"g", "bug0.go")
 	run("go", "tool", a+"g", "bug1.go")
 	run("go", "tool", a+"g", "bug2.go")
-	run(errchk, "go", "tool", a+"g", "-e", "bug3.go")
+	errchk("bug3.go", "go", "tool", a+"g", "-e", "bug3.go")
 	run("go", "tool", a+"l", "bug2."+a)
 	run(fmt.Sprintf(".%c%s.out", filepath.Separator, a))
 
@@ -54,4 +57,196 @@ func check(err error) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func errchk(filename string, name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		fmt.Println("expected compilation failure:", filename)
+		os.Exit(1)
+	}
+	err = errorCheck(string(out), filename, filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// Code from here down is copied and pasted from run.go.
+
+var (
+	errRx       = regexp.MustCompile(`// (?:GC_)?ERROR (.*)`)
+	errQuotesRx = regexp.MustCompile(`"([^"]*)"`)
+	lineRx      = regexp.MustCompile(`LINE(([+-])([0-9]+))?`)
+)
+
+type wantedError struct {
+	reStr   string
+	re      *regexp.Regexp
+	lineNum int
+	file    string
+	prefix  string
+}
+
+func errorCheck(outStr string, fullshort ...string) (err error) {
+	var errs []error
+
+	var out []string
+	// 6g error messages continue onto additional lines with leading tabs.
+	// Split the output at the beginning of each line that doesn't begin with a tab.
+	for _, line := range strings.Split(outStr, "\n") {
+		if strings.HasSuffix(line, "\r") { // remove '\r', output by compiler on windows
+			line = line[:len(line)-1]
+		}
+		if strings.HasPrefix(line, "\t") {
+			out[len(out)-1] += "\n" + line
+		} else if strings.HasPrefix(line, "go tool") {
+			continue
+		} else if strings.TrimSpace(line) != "" {
+			out = append(out, line)
+		}
+	}
+
+	// Cut directory name.
+	for i := range out {
+		for j := 0; j < len(fullshort); j += 2 {
+			full, short := fullshort[j], fullshort[j+1]
+			out[i] = strings.Replace(out[i], full, short, -1)
+		}
+	}
+
+	var want []wantedError
+	for j := 0; j < len(fullshort); j += 2 {
+		full, short := fullshort[j], fullshort[j+1]
+		want = append(want, wantedErrors(full, short)...)
+	}
+
+	for _, we := range want {
+		var errmsgs []string
+		errmsgs, out = partitionStrings(we.prefix, out)
+		if len(errmsgs) == 0 {
+			errs = append(errs, fmt.Errorf("%s:%d: missing error %q", we.file, we.lineNum, we.reStr))
+			continue
+		}
+		matched := false
+		n := len(out)
+		for _, errmsg := range errmsgs {
+			if we.re.MatchString(errmsg) {
+				matched = true
+			} else {
+				out = append(out, errmsg)
+			}
+		}
+		if !matched {
+			errs = append(errs, fmt.Errorf("%s:%d: no match for %#q in:\n\t%s", we.file, we.lineNum, we.reStr, strings.Join(out[n:], "\n\t")))
+			continue
+		}
+	}
+
+	if len(out) > 0 {
+		errs = append(errs, fmt.Errorf("Unmatched Errors:"))
+		for _, errLine := range out {
+			errs = append(errs, fmt.Errorf("%s", errLine))
+		}
+	}
+
+	if len(errs) == 0 {
+		return nil
+	}
+	if len(errs) == 1 {
+		return errs[0]
+	}
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "\n")
+	for _, err := range errs {
+		fmt.Fprintf(&buf, "%s\n", err.Error())
+	}
+	return errors.New(buf.String())
+
+}
+
+func wantedErrors(file, short string) (errs []wantedError) {
+	cache := make(map[string]*regexp.Regexp)
+
+	src, _ := ioutil.ReadFile(file)
+	for i, line := range strings.Split(string(src), "\n") {
+		lineNum := i + 1
+		if strings.Contains(line, "////") {
+			// double comment disables ERROR
+			continue
+		}
+		m := errRx.FindStringSubmatch(line)
+		if m == nil {
+			continue
+		}
+		all := m[1]
+		mm := errQuotesRx.FindAllStringSubmatch(all, -1)
+		if mm == nil {
+			log.Fatalf("%d: invalid errchk line: %s", lineNum, line)
+		}
+		for _, m := range mm {
+			rx := lineRx.ReplaceAllStringFunc(m[1], func(m string) string {
+				n := lineNum
+				if strings.HasPrefix(m, "LINE+") {
+					delta, _ := strconv.Atoi(m[5:])
+					n += delta
+				} else if strings.HasPrefix(m, "LINE-") {
+					delta, _ := strconv.Atoi(m[5:])
+					n -= delta
+				}
+				return fmt.Sprintf("%s:%d", short, n)
+			})
+			re := cache[rx]
+			if re == nil {
+				var err error
+				re, err = regexp.Compile(rx)
+				if err != nil {
+					log.Fatalf("%d: invalid regexp in ERROR line: %v", lineNum, err)
+				}
+				cache[rx] = re
+			}
+			prefix := fmt.Sprintf("%s:%d", short, lineNum)
+			errs = append(errs, wantedError{
+				reStr:   rx,
+				re:      re,
+				prefix:  prefix,
+				lineNum: lineNum,
+				file:    short,
+			})
+		}
+	}
+
+	return
+}
+
+func partitionStrings(prefix string, strs []string) (matched, unmatched []string) {
+	for _, s := range strs {
+		if matchPrefix(s, prefix) {
+			matched = append(matched, s)
+		} else {
+			unmatched = append(unmatched, s)
+		}
+	}
+	return
+}
+
+// matchPrefix reports whether s is of the form ^(.*/)?prefix(:|[),
+// That is, it needs the file name prefix followed by a : or a [,
+// and possibly preceded by a directory name.
+func matchPrefix(s, prefix string) bool {
+	i := strings.Index(s, ":")
+	if i < 0 {
+		return false
+	}
+	j := strings.LastIndex(s[:i], "/")
+	s = s[j+1:]
+	if len(s) <= len(prefix) || s[:len(prefix)] != prefix {
+		return false
+	}
+	switch s[len(prefix)] {
+	case '[', ':':
+		return true
+	}
+	return false
 }
