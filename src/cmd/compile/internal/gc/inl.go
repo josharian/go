701746a5a68e15ff173b45b91a5958818036a2c4
Josh Bleecher Snyder <josharian@gmail.com>
2016-04-26 22:21:12 -0700

    150: 9301548
    name       old time/op      new time/op      delta
    Template        286ms ± 4%       272ms ± 4%   -4.99%        (p=0.000 n=19+18)
    Unicode         145ms ± 6%       149ms ± 5%   +2.82%        (p=0.003 n=20+19)
    GoTypes         926ms ± 3%       891ms ± 3%   -3.73%        (p=0.000 n=19+19)
    Compiler        4.63s ± 4%       4.37s ± 1%   -5.59%        (p=0.000 n=20+18)
    MakeBash        37.6s ± 2%       37.6s ± 1%     ~           (p=0.758 n=20+20)
    
    name       old user-ns/op   new user-ns/op   delta
    Template   379user-ms ± 5%  396user-ms ± 6%   +4.61%        (p=0.000 n=19+20)
    Unicode    212user-ms ± 4%  236user-ms ± 4%  +11.76%        (p=0.000 n=20+19)
    GoTypes    1.29user-s ± 2%  1.32user-s ± 3%   +2.22%        (p=0.000 n=20+20)
    Compiler   6.78user-s ± 3%  6.51user-s ± 3%   -3.98%        (p=0.000 n=20+20)
    
    name       old alloc/op     new alloc/op     delta
    Template       49.2MB ± 0%      44.9MB ± 0%   -8.87%        (p=0.000 n=20+20)
    Unicode        37.3MB ± 0%      37.3MB ± 0%   +0.01%        (p=0.024 n=20+20)
    GoTypes         166MB ± 0%       153MB ± 0%   -8.13%        (p=0.000 n=20+19)
    Compiler        723MB ± 0%       653MB ± 0%   -9.68%        (p=0.000 n=20+20)
    
    name       old allocs/op    new allocs/op    delta
    Template         441k ± 0%        417k ± 0%   -5.43%        (p=0.000 n=20+20)
    Unicode          360k ± 0%        360k ± 0%     ~           (p=0.449 n=20+20)
    GoTypes         1.34M ± 0%       1.27M ± 0%   -5.56%        (p=0.000 n=20+20)
    Compiler        5.21M ± 0%       4.79M ± 0%   -8.05%        (p=0.000 n=20+20)
    
    name       old text-bytes   new text-bytes   delta
    HelloSize        586k ± 0%        586k ± 0%     ~     (all samples are equal)
    CmdGoSize       5.78M ± 0%       5.78M ± 0%     ~     (all samples are equal)
    
    name       old data-bytes   new data-bytes   delta
    HelloSize        130k ± 0%        130k ± 0%     ~     (all samples are equal)
    CmdGoSize        307k ± 0%        307k ± 0%     ~     (all samples are equal)
    
    name       old exe-bytes    new exe-bytes    delta
    HelloSize        952k ± 0%        952k ± 0%     ~     (all samples are equal)
    CmdGoSize       9.30M ± 0%       9.30M ± 0%     ~     (all samples are equal)
    
    Change-Id: Ice3eb8faed94296d0f4afdafcfa866cfc360e59b

--- src/cmd/compile/internal/gc/inl.go
+++ src/cmd/compile/internal/gc/inl.go
@@ -128,7 +128,7 @@ func caninl(fn *Node) {
 		return
 	}
 
-	const maxBudget = 120
+	const maxBudget = 150
 	budget := int32(maxBudget) // allowed hairyness
 	if ishairylist(fn.Nbody, &budget) || budget < 0 || Debug_inlcost != 0 {
 		return
