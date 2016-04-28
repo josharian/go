0f419e0d302ed7d73bdf56fa57b94d1b3a3e1762
Josh Bleecher Snyder <josharian@gmail.com>
2016-04-27 19:20:46 -0700

    600?!
    (cv) (tip)16:27:27 /Volumes/Data/src/go.tip/test/bench/go1 (inl-instrument)$ ls -l `which go` # master
    -rwxr-xr-x  1 josh  staff  9411132 Apr 27 16:42 /Volumes/Data/src/go.tip/bin/go
    (cv) (tip)19:12:30 /Volumes/Data/src/go.tip/test/bench/go1 (inl-instrument)$ ls -l `which go` # 200
    -rwxr-xr-x  1 josh  staff  9270428 Apr 27 19:13 /Volumes/Data/src/go.tip/bin/go
    (cv) (tip)19:14:11 /Volumes/Data/src/go.tip/test/bench/go1 (inl-instrument)$ ls -l `which go` # 300
    -rwxr-xr-x  1 josh  staff  9332332 Apr 27 19:15 /Volumes/Data/src/go.tip/bin/go
    (cv) (tip)19:15:59 /Volumes/Data/src/go.tip/test/bench/go1 (inl-instrument *)$ ls -l `which go` # 400
    -rwxr-xr-x  1 josh  staff  9346972 Apr 27 19:17 /Volumes/Data/src/go.tip/bin/go
    (cv) (tip)19:17:36 /Volumes/Data/src/go.tip/test/bench/go1 (inl-instrument *)$ ls -l `which go` # 600
    -rwxr-xr-x  1 josh  staff  9407116 Apr 27 19:19 /Volumes/Data/src/go.tip/bin/go
    
    name       old time/op      new time/op      delta
    Template        377ms ±15%       380ms ±14%    ~           (p=0.880 n=14+15)
    Unicode         166ms ±15%       166ms ± 9%    ~           (p=0.683 n=15+12)
    GoTypes         1.14s ± 9%       1.14s ±11%    ~           (p=0.806 n=15+15)
    Compiler        5.77s ± 8%       5.56s ± 9%    ~           (p=0.056 n=15+15)
    
    name       old user-ns/op   new user-ns/op   delta
    Template   432user-ms ±13%  428user-ms ± 9%    ~           (p=0.856 n=15+13)
    Unicode    220user-ms ± 7%  222user-ms ± 9%    ~           (p=0.762 n=13+13)
    GoTypes    1.46user-s ± 5%  1.44user-s ± 6%    ~           (p=0.267 n=15+15)
    Compiler   7.76user-s ± 8%  7.47user-s ± 9%  -3.82%        (p=0.013 n=15+15)
    
    name       old alloc/op     new alloc/op     delta
    Template       51.6MB ± 0%      50.3MB ± 0%  -2.41%        (p=0.000 n=15+15)
    Unicode        37.2MB ± 0%      37.2MB ± 0%    ~           (p=0.624 n=15+15)
    GoTypes         166MB ± 0%       165MB ± 0%  -0.75%        (p=0.000 n=15+15)
    Compiler        759MB ± 0%       718MB ± 0%  -5.41%        (p=0.000 n=15+15)
    
    name       old allocs/op    new allocs/op    delta
    Template         453k ± 0%        446k ± 0%  -1.53%        (p=0.000 n=15+15)
    Unicode          356k ± 0%        356k ± 0%    ~           (p=0.560 n=15+15)
    GoTypes         1.32M ± 0%       1.31M ± 0%  -0.71%        (p=0.000 n=15+15)
    Compiler        5.36M ± 0%       5.13M ± 0%  -4.33%        (p=0.000 n=15+15)
    
    name       old text-bytes   new text-bytes   delta
    HelloSize        578k ± 0%        578k ± 0%    ~     (all samples are equal)
    CmdGoSize       5.94M ± 0%       5.94M ± 0%    ~     (all samples are equal)
    
    name       old data-bytes   new data-bytes   delta
    HelloSize        130k ± 0%        130k ± 0%    ~     (all samples are equal)
    CmdGoSize        307k ± 0%        307k ± 0%    ~     (all samples are equal)
    
    name       old exe-bytes    new exe-bytes    delta
    HelloSize        930k ± 0%        930k ± 0%    ~     (all samples are equal)
    CmdGoSize       9.41M ± 0%       9.41M ± 0%    ~     (all samples are equal)
    
    
    Change-Id: I3cdeedf785c09c533d22fd7d025e04c976d8194c

--- src/cmd/compile/internal/gc/inl.go
+++ src/cmd/compile/internal/gc/inl.go
@@ -128,7 +128,7 @@ func caninl(fn *Node) {
 		return
 	}
 
-	const maxBudget = 200
+	const maxBudget = 600
 	budget := int32(maxBudget) // allowed hairyness
 	if ishairylist(fn.Nbody, &budget) || budget < 0 || Debug_inlcost != 0 {
 		return
