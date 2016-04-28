0e3815cfafcf146070e658f19e408d3fc0712551
Josh Bleecher Snyder <josharian@gmail.com>
2016-04-28 09:46:10 -0700

    650
    Change-Id: I0ee06a637f6cd0af455414bb50b164dc75ff8489

--- src/cmd/compile/internal/gc/inl.go
+++ src/cmd/compile/internal/gc/inl.go
@@ -128,7 +128,7 @@ func caninl(fn *Node) {
 		return
 	}
 
-	const maxBudget = 350
+	const maxBudget = 650
 	budget := int32(maxBudget) // allowed hairyness
 	if ishairylist(fn.Nbody, &budget) || budget < 0 || Debug_inlcost != 0 {
 		return
