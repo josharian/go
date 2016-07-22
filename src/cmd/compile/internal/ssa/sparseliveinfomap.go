400140ebe1d129d7b77778e22f754d80d3f4efef
Josh Bleecher Snyder <josharian@gmail.com>
2016-07-22 13:42:09 -0700

    copy-on-write
    Change-Id: I4bb36e38ac9e978ba6a0e508b1f54b864b34fb50

--- src/cmd/compile/internal/ssa/sparseliveinfomap.go
+++ src/cmd/compile/internal/ssa/sparseliveinfomap.go
@@ -10,49 +10,61 @@ package ssa
 type sparseLiveInfoMap struct {
 	dense  []liveInfo
 	sparse []int32
+	// copy-on-write support
+	densecopy []liveInfo
+	usecopy   bool
 }
 
 // newSparseLiveInfoMap returns a sparseLiveInfoMap
 // that maps from IDs to distances.
 func newSparseLiveInfoMap(n int) *sparseLiveInfoMap {
-	return &sparseLiveInfoMap{
-		dense:  nil,
-		sparse: make([]int32, n),
-	}
+	return &sparseLiveInfoMap{sparse: make([]int32, n)}
 }
 
 func (s *sparseLiveInfoMap) size() int {
-	return len(s.dense)
+	return len(s.contents())
 }
 
 func (s *sparseLiveInfoMap) contains(k ID) bool {
 	i := s.sparse[k]
-	return i < int32(len(s.dense)) && s.dense[i].ID == k
+	dense := s.contents()
+	return i < int32(len(dense)) && dense[i].ID == k
 }
 
 // get returns the dist for ID k, or -1 if k does not appear in the map.
 func (s *sparseLiveInfoMap) get(k ID) int32 {
 	i := s.sparse[k]
-	if i < int32(len(s.dense)) && s.dense[i].ID == k {
-		return s.dense[i].dist
+	dense := s.contents()
+	if i < int32(len(dense)) && dense[i].ID == k {
+		return dense[i].dist
 	}
 	return -1
 }
 
 func (s *sparseLiveInfoMap) setto(all []liveInfo) {
-	for cap(s.dense) < len(all) {
-		s.dense = s.dense[:len(s.dense)]
-		s.dense = append(s.dense, liveInfo{})
-	}
-	s.dense = s.dense[:len(all)]
-	copy(s.dense, all)
+	s.usecopy = true
+	s.densecopy = all
 	for i, e := range all {
 		s.sparse[e.ID] = int32(i)
 	}
-	return
+}
+
+func (s *sparseLiveInfoMap) copyForWrite() {
+	if !s.usecopy {
+		// already copied
+		return
+	}
+	for cap(s.dense) < len(s.densecopy) {
+		s.dense = s.dense[:len(s.dense)]
+		s.dense = append(s.dense, liveInfo{})
+	}
+	s.dense = s.dense[:len(s.densecopy)]
+	copy(s.dense, s.densecopy)
+	s.usecopy = false
 }
 
 func (s *sparseLiveInfoMap) set(k ID, v int32) {
+	s.copyForWrite()
 	i := s.sparse[k]
 	if i < int32(len(s.dense)) && s.dense[i].ID == k {
 		s.dense[i].dist = v
@@ -63,5 +75,8 @@ func (s *sparseLiveInfoMap) set(k ID, v int32) {
 }
 
 func (s *sparseLiveInfoMap) contents() []liveInfo {
+	if s.usecopy {
+		return s.densecopy
+	}
 	return s.dense
 }
