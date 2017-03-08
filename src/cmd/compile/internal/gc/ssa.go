f1fb93c5e04a6611f4f6677ebf8957f91e4db113
Josh Bleecher Snyder <josharian@gmail.com>
2017-03-08 12:50:00 -0800

    cmd/compile: place runtime call SP offsets in entry block
    Matches what we do elsewhere. No reason not to.
    Maybe even makes optimizations better?
    
    Change-Id: Ib10db07fe4e7c87db34f9703727afe4f4cc5c984

--- src/cmd/compile/internal/gc/ssa.go
+++ src/cmd/compile/internal/gc/ssa.go
@@ -3364,7 +3364,7 @@ func (s *state) rtcall(fn *obj.LSym, returns bool, results []*Type, args ...*ssa
 	for _, arg := range args {
 		t := arg.Type
 		off = Rnd(off, t.Alignment())
-		ptr := s.newValue1I(ssa.OpOffPtr, t.PtrTo(), off, s.sp)
+		ptr := s.entryNewValue1I(ssa.OpOffPtr, t.PtrTo(), off, s.sp)
 		size := t.Size()
 		s.vars[&memVar] = s.newValue3I(ssa.OpStore, ssa.TypeMem, size, ptr, arg, s.mem())
 		off += size
@@ -3395,7 +3395,7 @@ func (s *state) rtcall(fn *obj.LSym, returns bool, results []*Type, args ...*ssa
 	res := make([]*ssa.Value, len(results))
 	for i, t := range results {
 		off = Rnd(off, t.Alignment())
-		ptr := s.newValue1I(ssa.OpOffPtr, ptrto(t), off, s.sp)
+		ptr := s.entryNewValue1I(ssa.OpOffPtr, ptrto(t), off, s.sp)
 		res[i] = s.newValue2(ssa.OpLoad, t, ptr, s.mem())
 		off += t.Size()
 	}
