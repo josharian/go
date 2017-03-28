3b318cd90eebb6d241804483fe9b01c82164b1f0
Josh Bleecher Snyder <josharian@gmail.com>
2017-03-28 07:13:07 -0700

    hack in walk.go to eagerly dowidth all types
    Change-Id: I02d16116985bb220ccf35d2fed8f87a2e2231832

--- src/cmd/compile/internal/gc/walk.go
+++ src/cmd/compile/internal/gc/walk.go
@@ -460,6 +460,14 @@ func walkexpr(n *Node, init *Nodes) *Node {
 		return n
 	}
 
+	if n.Type != nil {
+		switch n.Type.Etype {
+		case TBLANK, TNIL, TIDEAL:
+		default:
+			dowidth(n.Type)
+		}
+	}
+
 	if init == &n.Ninit {
 		// not okay to use n->ninit when walking n,
 		// because we might replace n with some other node
