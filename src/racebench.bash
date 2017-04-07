334c3a71a205f06919967cc56fa62feb24d28ec3
Josh Bleecher Snyder <josharian@gmail.com>
2017-04-07 10:16:51 -0700

    add racecheck.bash and racebench.bash
    Change-Id: I45225f9dbf955ec8194f78c2b00bd4f7a9c52616

---
+++ src/racebench.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set +v
+
+compilecmp -n 5
+compilecmp -n 50 -cpu
+
+compilecmp -n 5 -beforeflags="-c=1" -afterflags="-c=2" HEAD HEAD
+compilecmp -n 50 -cpu -beforeflags="-c=1" -afterflags="-c=2" HEAD HEAD
+
+compilecmp -n 5 -beforeflags="-c=1" -afterflags="-c=4" HEAD HEAD
+compilecmp -n 50 -cpu -beforeflags="-c=1" -afterflags="-c=4" HEAD HEAD
+
+compilecmp -n 5 -beforeflags="-c=1" -afterflags="-c=8" HEAD HEAD
+compilecmp -n 50 -cpu -beforeflags="-c=1" -afterflags="-c=8" HEAD HEAD
+
+compilecmp -n 5 -beforeflags="-c=1" -afterflags="-c=16" HEAD HEAD
+compilecmp -n 50 -cpu -beforeflags="-c=1" -afterflags="-c=16" HEAD HEAD
+
+
+compilecmp -n 30 -cpu -all -run=StdCmd
+compilecmp -n 30 -cpu -all -run=StdCmd -beforeflags="-c=1" -afterflags="-c=2" HEAD HEAD
+compilecmp -n 30 -cpu -all -run=StdCmd -beforeflags="-c=1" -afterflags="-c=4" HEAD HEAD
+compilecmp -n 30 -cpu -all -run=StdCmd -beforeflags="-c=1" -afterflags="-c=8" HEAD HEAD
+compilecmp -n 30 -cpu -all -run=StdCmd -beforeflags="-c=1" -afterflags="-c=16" HEAD HEAD
