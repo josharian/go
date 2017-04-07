66f6683a2ae59d68655da117b57783f64a7ca621
Josh Bleecher Snyder <josharian@gmail.com>
2017-04-07 10:16:51 -0700

    add racecheck.bash
    Change-Id: Icd18b6a94ebd3d40e1d6f0375f36d8a216d085b8

---
+++ src/racecheck.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "all.bash"
+./all.bash
+echo "install race cmd/compile"
+go install -race cmd/compile
+echo "HOST"
+go build -a -gcflags="-c=16" std cmd
+
+echo "linux/ppc64"
+GOOS=linux GOARCH=ppc64 go build -a -gcflags="-c=16" std cmd 
+
+echo "linux/ppc64le"
+GOOS=linux GOARCH=ppc64le go build -a -gcflags="-c=16" std cmd 
+
+
+echo "linux/s390x"
+GOOS=linux GOARCH=s390x go build -a -gcflags="-c=16" std cmd 
+echo "linux/arm64"
+GOOS=linux GOARCH=arm64 go build -a -gcflags="-c=16" std cmd 
+echo "linux/mips"
+GOOS=linux GOARCH=mips go build -a -gcflags="-c=16" std cmd 
+echo "linux/arm"
+GOOS=linux GOARCH=arm go build -a -gcflags="-c=16" std cmd 
+echo "nacl/arm"
+GOOS=nacl GOARCH=arm go build -a -gcflags="-c=16" std cmd 
+echo "linux/arm5"
+GOOS=linux GOARCH=arm GOARM=5 go build -a -gcflags="-c=16" std cmd 
+echo "linux/arm6"
+GOOS=linux GOARCH=arm GOARM=6 go build -a -gcflags="-c=16" std cmd 
+echo "linux/arm7"
+GOOS=linux GOARCH=arm GOARM=7 go build -a -gcflags="-c=16" std cmd 
+echo "nacl/arm5"
+GOOS=nacl GOARCH=arm GOARM=5 go build -a -gcflags="-c=16" std cmd 
+echo "nacl/arm6"
+GOOS=nacl GOARCH=arm GOARM=6 go build -a -gcflags="-c=16" std cmd 
+echo "nacl/arm7"
+GOOS=nacl GOARCH=arm GOARM=7 go build -a -gcflags="-c=16" std cmd 
