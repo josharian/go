38c229b7180826d5e254e6ed00b51bebda330956
Josh Bleecher Snyder <josharian@gmail.com>
2017-04-19 16:55:05 -0700

    cmd/compile: introduce a data race
    DO NOT SUBMIT
    
    This is an intentionally introduced data race,
    to test/demo the previous cmd/dist CL.
    
    Change-Id: Id6f292eb09cd85a7281da41a4edd63eda7909708

--- src/cmd/compile/internal/gc/dcl.go
+++ src/cmd/compile/internal/gc/dcl.go
@@ -1064,7 +1064,7 @@ func funcsymname(s *types.Sym) string {
 // funcsym returns s·f.
 func funcsym(s *types.Sym) *types.Sym {
 	// Lock funcsymsmu immediately, so that it can also guard the package lookup.
-	funcsymsmu.Lock()
+	// funcsymsmu.Lock()
 	sf, existed := s.Pkg.LookupOK(funcsymname(s))
 	// Don't export s·f when compiling for dynamic linking.
 	// When dynamically linking, the necessary function
@@ -1073,7 +1073,7 @@ func funcsym(s *types.Sym) *types.Sym {
 	if !Ctxt.Flag_dynlink && !existed {
 		funcsyms = append(funcsyms, s)
 	}
-	funcsymsmu.Unlock()
+	// funcsymsmu.Unlock()
 	return sf
 }
 
