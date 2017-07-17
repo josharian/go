6291859116061095e49f5886e7398a4b42da0d33
Josh Bleecher Snyder <josharian@gmail.com>
2017-07-16 17:52:30 -1000

    add grow toggle to benchmark

--- src/runtime/mapspeed_test.go
+++ src/runtime/mapspeed_test.go
@@ -310,14 +310,23 @@ func BenchmarkSmallKeyMap(b *testing.B) {
 }
 
 func BenchmarkMapPopulate(b *testing.B) {
-	for size := 1; size < 1000000; size *= 10 {
-		b.Run(strconv.Itoa(size), func(b *testing.B) {
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				m := make(map[int]bool)
-				for j := 0; j < size; j++ {
-					m[j] = true
-				}
+	for _, grow := range [...]bool{false, true} {
+		b.Run(fmt.Sprintf("grow=%v", grow), func(b *testing.B) {
+			for size := 1; size < 1000000; size *= 10 {
+				b.Run(strconv.Itoa(size), func(b *testing.B) {
+					b.ReportAllocs()
+					for i := 0; i < b.N; i++ {
+						var m map[int]bool
+						if grow {
+							m = make(map[int]bool)
+						} else {
+							m = make(map[int]bool, size)
+						}
+						for j := 0; j < size; j++ {
+							m[j] = true
+						}
+					}
+				})
 			}
 		})
 	}
