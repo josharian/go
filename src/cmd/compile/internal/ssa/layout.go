2a3bec301c0c183659717dafe648abb4c8189466
Josh Bleecher Snyder <josharian@gmail.com>
2018-02-24 08:22:30 -0800

    print some stuff
     69.44% 452318 SUCC 1
     22.02% 143448 SUCC 0
      4.54% 29573 diamond 1 1
      0.64%  4197 diamond 1 0
      0.36%  2325 diamond 2 2
      0.35%  2286 diamond 0 1
      0.29%  1882 diamond 5 0
      0.23%  1508 diamond 10 0
      0.19%  1212 diamond 2 0
      0.18%  1192 diamond 3 0
      0.17%  1106 diamond 0 0
      0.14%   899 diamond 8 0
      0.13%   862 diamond 4 0
      0.09%   607 diamond 6 0
      0.09%   599 diamond 0 2
      0.08%   553 diamond 7 0
      0.08%   522 diamond 5 1
      0.07%   451 diamond 0 3
      0.07%   442 diamond 3 3
      0.04%   288 diamond 5 4
      0.04%   279 diamond 9 0
      0.03%   219 diamond 11 0
      0.03%   212 diamond 4 4
      0.03%   199 diamond 0 6
      0.03%   191 diamond 12 0
      0.02%   151 diamond 14 0
      0.02%   134 diamond 5 5
      0.02%   112 diamond 1 2
      0.02%   106 diamond 1 3

--- src/cmd/compile/internal/ssa/layout.go
+++ src/cmd/compile/internal/ssa/layout.go
@@ -4,6 +4,11 @@
 
 package ssa
 
+import (
+	"fmt"
+	"os"
+)
+
 // layout orders basic blocks in f with the goal of minimizing control flow instructions.
 // After this phase returns, the order of f.Blocks matters and is the order
 // in which those blocks will appear in the assembly output.
@@ -99,6 +104,11 @@ blockloop:
 		// Pick among the successor blocks that have not been scheduled yet.
 
 		// Detect diamonds
+		if len(b.Succs) != 2 {
+			if os.Getenv("J") != "" {
+				fmt.Printf("SUCC %d\n", len(b.Succs))
+			}
+		}
 		if detectDiamonds && len(b.Succs) == 2 {
 			s0 := b.Succs[0].b
 			s1 := b.Succs[1].b
@@ -106,9 +116,9 @@ blockloop:
 				s0s := s0.Succs[0].b
 				s1s := s1.Succs[0].b
 				if s0s == s1s && !scheduled[s0s.ID] && !scheduled[s1s.ID] {
-					// if os.Getenv("J") != "" {
-					// 	fmt.Printf("diamond\n")
-					// }
+					if os.Getenv("J") != "" {
+						fmt.Printf("diamond %d %d\n", len(s0.Values), len(s1.Values))
+					}
 					// Use likely direction if we have it.
 					if b.Likely != BranchLikely {
 						s0, s1 = s1, s0
