373bdd27b20a3b82d62b5921d27e1159702e5923
Josh Bleecher Snyder <josharian@gmail.com>
2018-03-15 12:20:45 -0700

    shrink sparse slice
    Change-Id: Ica0df009e938efdaef9a400edb6857ae5c207184

--- src/cmd/compile/internal/ssa/sparseset.go
+++ src/cmd/compile/internal/ssa/sparseset.go
@@ -4,22 +4,39 @@
 
 package ssa
 
+import "strconv"
+
 // from http://research.swtch.com/sparse
 // in turn, from Briggs and Torczon
 
 type sparseSet struct {
-	dense  []ID
-	sparse []int32
+	dense []ID
+	// exactly one of the sparse slices will be non-nil
+	sparse8  []uint8
+	sparse16 []uint16
+	sparse32 []int32
 }
 
 // newSparseSet returns a sparseSet that can represent
 // integers between 0 and n-1
 func newSparseSet(n int) *sparseSet {
-	return &sparseSet{dense: nil, sparse: make([]int32, n)}
+	if n < 1<<8 {
+		return &sparseSet{dense: nil, sparse8: make([]uint8, n)}
+	}
+	if n < 1<<16 {
+		return &sparseSet{dense: nil, sparse16: make([]uint16, n)}
+	}
+	return &sparseSet{dense: nil, sparse32: make([]int32, n)}
 }
 
 func (s *sparseSet) cap() int {
-	return len(s.sparse)
+	if s.sparse8 != nil {
+		return len(s.sparse8)
+	}
+	if s.sparse16 != nil {
+		return len(s.sparse16)
+	}
+	return len(s.sparse32)
 }
 
 func (s *sparseSet) size() int {
@@ -27,17 +44,17 @@ func (s *sparseSet) size() int {
 }
 
 func (s *sparseSet) contains(x ID) bool {
-	i := s.sparse[x]
-	return i < int32(len(s.dense)) && s.dense[i] == x
+	i := s.sparseAt(x)
+	return i < len(s.dense) && s.dense[i] == x
 }
 
 func (s *sparseSet) add(x ID) {
-	i := s.sparse[x]
-	if i < int32(len(s.dense)) && s.dense[i] == x {
+	i := s.sparseAt(x)
+	if i < len(s.dense) && s.dense[i] == x {
 		return
 	}
 	s.dense = append(s.dense, x)
-	s.sparse[x] = int32(len(s.dense)) - 1
+	s.setSparseAt(x, len(s.dense)-1)
 }
 
 func (s *sparseSet) addAll(a []ID) {
@@ -53,11 +70,11 @@ func (s *sparseSet) addAllValues(a []*Value) {
 }
 
 func (s *sparseSet) remove(x ID) {
-	i := s.sparse[x]
-	if i < int32(len(s.dense)) && s.dense[i] == x {
+	i := s.sparseAt(x)
+	if i < len(s.dense) && s.dense[i] == x {
 		y := s.dense[len(s.dense)-1]
 		s.dense[i] = y
-		s.sparse[y] = i
+		s.setSparseAt(y, i)
 		s.dense = s.dense[:len(s.dense)-1]
 	}
 }
@@ -76,4 +93,38 @@ func (s *sparseSet) clear() {
 
 func (s *sparseSet) contents() []ID {
 	return s.dense
+}
+
+func (s *sparseSet) sparseAt(x ID) int {
+	if s.sparse8 != nil {
+		return int(s.sparse8[x])
+	}
+	if s.sparse16 != nil {
+		return int(s.sparse16[x])
+	}
+	return int(s.sparse32[x])
+}
+
+func (s *sparseSet) setSparseAt(x ID, v int) {
+	if s.sparse8 != nil {
+		u := uint8(v)
+		if int(u) != v {
+			panic("bad sparse8 index " + strconv.Itoa(v))
+		}
+		s.sparse8[x] = u
+		return
+	}
+	if s.sparse16 != nil {
+		u := uint16(v)
+		if int(u) != v {
+			panic("bad sparse16 index " + strconv.Itoa(v))
+		}
+		s.sparse16[x] = u
+		return
+	}
+	u := int32(v)
+	if int(u) != v {
+		panic("bad sparse32 index " + strconv.Itoa(v))
+	}
+	s.sparse32[x] = u
 }
