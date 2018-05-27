d610233727bd84fc82ef3790ab323ac61599bd0f
Josh Bleecher Snyder <josharian@gmail.com>
2018-05-27 08:07:52 -0700

    cmd/compile: disable magic division during first opt pass
    Magic division disrupts our ability to detect
    obvious boundededness during the opt pass.
    Usually we avoid order dependence by writing
    rewrite rules to incorporate the work done by other rules.
    In the case of magic division, that is infeasible;
    the optimized form is too hard to detect and invert.
    Instead, defer magic division to the second opt pass.
    This is ugly, but it appears to work well in practice.
    
    Change-Id: I0634477ebe20b9afb9a0e7a3b65d6786d97f5268

--- src/cmd/compile/internal/ssa/compile.go
+++ src/cmd/compile/internal/ssa/compile.go
@@ -366,7 +366,7 @@ var passes = [...]pass{
 	{name: "early deadcode", fn: deadcode}, // remove generated dead code to avoid doing pointless work during opt
 	{name: "short circuit", fn: shortcircuit},
 	{name: "decompose user", fn: decomposeUser, required: true},
-	{name: "opt", fn: opt, required: true},               // TODO: split required rules and optimizing rules
+	{name: "opt", fn: opt, required: true},               // NB: some generic rewrite rules know the name of this pass; TODO: split required rules and optimizing rules
 	{name: "zero arg cse", fn: zcse, required: true},     // required to merge OpSB values
 	{name: "opt deadcode", fn: deadcode, required: true}, // remove any blocks orphaned during opt
 	{name: "generic cse", fn: cse},
