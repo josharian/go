01bd66572216a49374becf194b74a0fca59d6384
Josh Bleecher Snyder <josharian@gmail.com>
2018-10-20 19:59:27 -0700

    cmd/compile: optimize A->B->C Moves that include VarDefs
    We have an existing optimization that recognizes
    memory moves of the form A -> B -> C and converts
    them into A -> C, in the hopes that the store to
    B will be end up being dead and thus eliminated.
    
    However, when A, B, and C are large types,
    the front end sometimes emits VarDef ops for the moves.
    This change adds an optimization to match that pattern.
    
    This required changing an old compiler test.
    The test assumed that a temporary was required
    to deal with a large return value.
    With this optimization in place, that temporary
    ended up being eliminated.
    It has been replaced by a test in which the
    relevant memory move is not in-place.
    
    Triggers 716 times during 'go build -a std cmd'.
    
    Cuts 12k off cmd/go.
    
    name        old object-bytes  new object-bytes  delta
    Template          507kB ± 0%        506kB ± 0%  -0.18%  (p=0.008 n=5+5)
    Unicode           225kB ± 0%        225kB ± 0%    ~     (all equal)
    GoTypes          1.85MB ± 0%       1.85MB ± 0%    ~     (all equal)
    Flate             328kB ± 0%        328kB ± 0%    ~     (all equal)
    GoParser          402kB ± 0%        402kB ± 0%  -0.00%  (p=0.008 n=5+5)
    Reflect          1.41MB ± 0%       1.41MB ± 0%  -0.20%  (p=0.008 n=5+5)
    Tar               457kB ± 0%        457kB ± 0%    ~     (all equal)
    XML               601kB ± 0%        599kB ± 0%  -0.21%  (p=0.008 n=5+5)
    
    Change-Id: I9b5f25c8663a0b772ad1ee51fa61f74b74d26dd3

--- test/fixedbugs/issue20780.go
+++ test/fixedbugs/issue20780.go
@@ -6,15 +6,13 @@
 
 // We have a limit of 1GB for stack frames.
 // Make sure we include the callee args section.
-// (The dispatch wrapper which implements (*S).f
-// copies the return value from f to a stack temp, then
-// from that stack temp to the return value of (*S).f.
-// It uses ~800MB for each section.)
 
 package main
 
-type S struct {
-	i interface {
-		f() [800e6]byte
-	}
+func f() (x [800e6]byte) {
+	g(1, x) // ensure that x gets copied to a new location on the stack
+	return
 }
+
+//go:noinline
+func g(byte, [800e6]byte) {}
