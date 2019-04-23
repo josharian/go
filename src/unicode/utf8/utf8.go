997ba8460eca5c8bc8ca9e33efc406c62aa2ce68
Josh Bleecher Snyder <josharian@gmail.com>
2019-04-23 13:19:28 -0700

    optimize DecodeRune
    Change-Id: I2cffa58716147acb11f2bbd7ef0adb3a5a29c763

--- src/unicode/utf8/utf8.go
+++ src/unicode/utf8/utf8.go
@@ -148,12 +148,10 @@ func FullRuneInString(s string) bool {
 // out of range, or is not the shortest possible UTF-8 encoding for the
 // value. No other validation is performed.
 func DecodeRune(p []byte) (r rune, size int) {
-	n := len(p)
-	if n < 1 {
+	if len(p) < 1 {
 		return RuneError, 0
 	}
-	p0 := p[0]
-	x := first[p0]
+	x := first[p[0]]
 	if x >= as {
 		// The following code simulates an additional check for x == xx and
 		// handling the ASCII and invalid cases accordingly. This mask-and-or
@@ -161,30 +159,36 @@ func DecodeRune(p []byte) (r rune, size int) {
 		mask := rune(x) << 31 >> 31 // Create 0x0000 or 0xFFFF.
 		return rune(p[0])&^mask | RuneError&mask, 1
 	}
-	sz := x & 7
+	size = int(x & 7)
 	accept := acceptRanges[x>>4]
-	if n < int(sz) {
-		return RuneError, 1
+	if len(p) < size {
+		goto err
 	}
-	b1 := p[1]
-	if b1 < accept.lo || accept.hi < b1 {
-		return RuneError, 1
+	if p[1] < accept.lo || accept.hi < p[1] {
+		goto err
 	}
-	if sz == 2 {
-		return rune(p0&mask2)<<6 | rune(b1&maskx), 2
+	r = rune(p[0]&mask2)<<6 | rune(p[1]&maskx)
+	if size <= 2 {
+		return
 	}
-	b2 := p[2]
-	if b2 < locb || hicb < b2 {
-		return RuneError, 1
+	if p[2] < locb || hicb < p[2] {
+		goto err
 	}
-	if sz == 3 {
-		return rune(p0&mask3)<<12 | rune(b1&maskx)<<6 | rune(b2&maskx), 3
+	r <<= 6
+	r |= rune(p[2] & maskx)
+	r &^= 1 << 16
+	if size <= 3 {
+		return
 	}
-	b3 := p[3]
-	if b3 < locb || hicb < b3 {
-		return RuneError, 1
+	if p[3] < locb || hicb < p[3] {
+		goto err
 	}
-	return rune(p0&mask4)<<18 | rune(b1&maskx)<<12 | rune(b2&maskx)<<6 | rune(b3&maskx), 4
+	r <<= 6
+	r |= rune(p[3] & maskx)
+	r &^= 1 << 21
+	return
+err:
+	return RuneError, 1
 }
 
 // DecodeRuneInString is like DecodeRune but its input is a string. If s is
